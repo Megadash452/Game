5962e632fba4039fef6022d7e20b3f9333e4fef9
marti <marticraft680@gmail.com>
2020-07-31 17:09:46 -0400

    issues with server.py

---
+++ server.py
@@ -0,0 +1,46 @@
+import socket
+from _thread import *
+import sys
+
+server = "192.168.1.9"
+port = 5551
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+
+def threaded_client(connection):
+    connection.send(str.encode("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = connection.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending : ", reply)
+
+            connection.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Lost connection")
+    connection.close()
+
+
+while True:
+    connection, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (connection,))
